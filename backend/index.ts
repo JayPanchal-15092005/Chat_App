ad0ef562e2e7b79204b88feac4c32f0f85de0a36
Jay_Panchal <trywebsiteapp56@gmail.com>
2026-03-30 18:47:23 +0530

    Implement socket server initialization and user authentication handling

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,17 @@
 import app from "./src/app.ts";
 import { connectDB } from "./src/config/database.ts";
+import { createServer } from "http";
+import { initializeSocket } from "./src/utils/socket.ts";
 
 const PORT = process.env.PORT || 3000;
 
+const httpServer = createServer(app);
+
+initializeSocket(httpServer);
+
 connectDB()
 .then(() => {
-    app.listen(PORT, () => {
+    httpServer.listen(PORT, () => {
         console.log("Server is running on PORT:", PORT);
     });
 })
