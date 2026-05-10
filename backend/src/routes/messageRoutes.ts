fa2416564fac787e940dd42e695839bddedab3ea
Jay_Panchal <trywebsiteapp56@gmail.com>
2026-05-10 14:55:01 +0530

    fixing the issues

--- backend/src/routes/messageRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { protectRoute } from "../middleware/auth.ts";
-import { getMessages } from "../controllers/messageController.ts";
-
-const router = Router();
-
-router.get("/chat/:chatId", protectRoute, getMessages);
-
-export default router;
--- backend/src/routes/userRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { protectRoute } from "../middleware/auth.ts";
-import { getUsers } from "../controllers/userController.ts";
-
-const router = Router();
-
-router.get("/", protectRoute, getUsers);
-
-export default router;
