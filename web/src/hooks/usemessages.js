a08034d638cde7d866e5eadab36ace04f9f859ee
Jay_Panchal <trywebsiteapp56@gmail.com>
2026-06-18 16:04:24 +0530

    fix: enhance chatId validation in useMessages hook

--- web/src/hooks/usemessages.js
+++ web/src/hooks/usemessages.js
@@ -16,6 +16,6 @@ export const useMessages = (chatId) => {
       });
       return data;
     },
-    enabled: !!chatId && !!token,
+    enabled: !!chatId && chatId !== "undefined" && chatId !== "null" && !!token,
   });
 };
